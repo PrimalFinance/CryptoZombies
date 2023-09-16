0c623e3958b68108eb81a635867ab84be47f38fc
PrimalFinance <primalfinance101@gmail.com>
2023-09-15 23:24:05 -0700

    Added chapter 3.

---
+++ Beginner_to_Intermediate/Lesson_04/Chapter_03/zombie_battles.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+// Import zombiehelper.sol file that is in the problem directory on the website.
+import "./zombiehelper.sol";
+
+// Create a contract "ZombieAttack" that inherits from ZombieHelper.
+contract ZombieAttack is ZombieHelper {
+
+}
