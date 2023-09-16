4d0eda8f5a4fb4e6829a8a19d283bbb9f5150c06
PrimalFinance <primalfinance101@gmail.com>
2023-09-15 23:53:04 -0700

    Added chapter 6.

---
+++ Beginner_to_Intermediate/Lesson_04/Lesson_07/more_refactoring.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.5.0 <0.6.0;
+
+import "./zombiefeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+    uint levelUpFee = 0.001 ether;
+
+    modifier aboveLevel(uint _level, uint _zombieId) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+
+    function withdraw() external onlyOwner {
+        address _owner = owner();
+        _owner.transfer(address(this).balance);
+    }
+
+    function setLevelUpFee(uint _fee) external onlyOwner {
+        levelUpFee = _fee;
+    }
+
+    function levelUp(uint _zombieId) external payable {
+        require(msg.value == levelUpFee);
+        zombies[_zombieId].level++;
+    }
+
+    // 1. Modify this function to use `ownerOf`:
+    function changeName(
+        uint _zombieId,
+        string calldata _newName
+    ) external aboveLevel(2, _zombieId) ownerOf(_zombieId) {
+        // We can remove this require check, because the "ownerOf" modifier will perform this check for us.
+        // REMOVED: require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].name = _newName;
+    }
+
+    // 2. Do the same with this function:
+    function changeDna(
+        uint _zombieId,
+        uint _newDna
+    ) external aboveLevel(20, _zombieId) ownerOf(_zombieId) {
+        // This line was removed for the same reasoning in the other function. The modifier ownerOf performs this require check, so we do not need to do it twice.
+        // REMOVED: require(msg.sender == zombieToOwner[_zombieId]);
+        zombies[_zombieId].dna = _newDna;
+    }
+
+    function getZombiesByOwner(
+        address _owner
+    ) external view returns (uint[] memory) {
+        uint[] memory result = new uint[](ownerZombieCount[_owner]);
+        uint counter = 0;
+        for (uint i = 0; i < zombies.length; i++) {
+            if (zombieToOwner[i] == _owner) {
+                result[counter] = i;
+                counter++;
+            }
+        }
+        return result;
+    }
+}
